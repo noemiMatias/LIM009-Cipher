8733f4772661648dc41b74c5e03698651ccacb1d
noemimatias <noemi.071213@gmail.com>
2019-02-25 13:09:04 -0500

    modique index html y js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
+const offset= document.getElementById("number");
 const textoCifrado = document.getElementById('text1');
 const textCifrado1= document.getElementById("text-cipher");
-textCifrado1.innerHTML = textoCifrado.value;
-
+const cifrar= document.getElementById("cifrar");
+cifrar.addEventListener("click",()=>{
+    textCifrado1.innerHTML = textoCifrado.value;// aca mensaje capturado lo imprimo con inner y aparece el valor de texto cifrado
+});
+// ( una variable =cifrar )cuando escuches el evento de cik haces esto = ahi se escribe la funcion =>
+const textoDecifrado2 = document.getElementById("text2");
+const textoDecipher2= document.getElementById("text-decipher");
+const descifrar= document.getElementById("descifrar");
+descifrar.addEventListener("click",()=>{
+    textoDecipher2.innerHTML = textoDecifrado2.value;
+} );
 
 
 

128bcb1e8a4e679bc4d15b3136781d0d1cd555c5
noemiMatias <noemi.071213@gmail.com>
2019-03-04 01:09:54 -0500

    dando diseño css y se empezo el cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -2,13 +2,18 @@ window.cipher = {
   encode: (offset,texto-cifrar) => {
     /* Acá va tu código */
     let nuevoMensaje=" ";
-    for(i =0;i<=texto-cifrar.length;i++){
-      if(texto-cifrar[i])
-    
-    if(i=65<=90);
-    console.log(encode);
-
-  },
+    for(let i =0;i <texto-cifrar.length;i++){
+      if(texto-cifrar[i].charCodeAt()>=65 && texto-cifrado[i].charCodeAt()<=90){
+        let numberAscii=((texto-cifrado[i].charCodeAt()-65+offset)%26+65);
+        let letterCipher=String.fromCharCode(numberAscii);
+        nuevoMensaje+=letterCipher;
+    }else{
+nuevoMensaje=nuevoMensaje+texto-cifrar[i];
+console.log(nuevoMensaje);
+    }
+  }
+  return nuevoMensaje;
+};
   decode: () => {
     /* Acá va tu código */
   }

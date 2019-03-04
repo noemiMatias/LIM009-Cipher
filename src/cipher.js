d569415cf39a4051b8dc57c4bb0b534efc7e395b
noemimatias <noemi.071213@gmail.com>
2019-03-04 13:23:53 -0500

    terine decifrado y cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,26 @@
 window.cipher = {
-  encode: () => {
-    /* Acá va tu código */
+  encode: (entrada,desplazamiento) => {
+  let codigoAscii;
+  let resultado="";
+
+  for(let i=0;i<entrada.length;i++){
+  codigoAscii=entrada[i].charCodeAt();
+  resultado+=String.fromCharCode((codigoAscii-65+desplazamiento)%26+65);
+  
+}
+ return resultado;
+
   },
-  decode: () => {
-    /* Acá va tu código */
+  decode: (entrada,desplazamiento) => {
+    let codigoAscii1;
+    let resultadoDes="";
+  
+    for(let i=0;i<entrada.length;i++){
+    codigoAscii1=entrada[i].charCodeAt();
+    resultadoDes+=String.fromCharCode((codigoAscii1-desplazamiento+65)%26+65);
+    
+    
   }
-};
+  return resultadoDes;
+}
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,37 @@
 
+let desplazamiento;
+let resultado;
+let entrada;
+let entrada2;
+let resultadoDes;
+
+document.getElementById("botonCifrar").addEventListener("click",()=>{
+     entrada=document.getElementById("textoCifrar").value;
+     desplazamiento=parseInt(document.getElementById("offset").value);
+    resultado=cipher.encode(entrada,desplazamiento);
+   document.getElementById("textoDecifrado").value=resultado;
+
+})
+
+
+document.getElementById("botonDecifrar").addEventListener("click",()=>{
+  entrada2= document.getElementById("textoDecifrado").value;
+  desplazamiento=parseInt(document.getElementById("offset").value);
+  resultadoDes=cipher.decode(entrada2,desplazamiento);
+     document.getElementById("textoCifrar").value=resultadoDes ;
+    
+ })
+
+
+
+
+
+
+
+
+
+
+
+
+//const textoDecifrado= document.getElementById("text2");
+                                           
